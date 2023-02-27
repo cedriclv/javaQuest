a2426d5fd3de183df22d49c0983257bf466cc703
CEDRIC LV <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-02-27 10:59:52 +0100

    first commit

---
+++ USerDetails/Address.java
@@ -0,0 +1,39 @@
+package USerDetails;
+
+public class Address {
+
+    private int number;
+    private String street;
+    private String city;
+    private String country;
+
+
+    public int getNumber() {
+        return this.number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
---
+++ USerDetails/User.java
@@ -0,0 +1,43 @@
+package USerDetails;
+
+import java.util.*;
+
+public class User {
+
+    private Date birthday;
+    private int size;
+    private Address address;
+    private List<Photo> photos = new ArrayList<Photo>();
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }    
+}
