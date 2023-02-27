6248a5a7887e81f162301f7d25227ae9def231aa
CEDRIC LV <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-02-27 11:39:38 +0100

    update

---
+++ USerDetails/Launch.java
@@ -0,0 +1,9 @@
+package USerDetails;
+
+public class Launch {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setSize(133);
+        System.out.println(user1.getSize());
+    }
+}
